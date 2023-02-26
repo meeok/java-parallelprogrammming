c50f7e396dcec28108d3edff7e322daea53b5f1e
Miranda Okpara <miranda59@gmail.com>
2023-02-26 11:51:34 +0100

    updating git ignore for idea files

---
+++ src/main/java/com/meeo/parallelprogramming/FJFramework/ReciprocalarraySum.java
@@ -0,0 +1,9 @@
+package com.meeo.parallelprogramming.FJFramework;
+
+/**
+ * @author MEEO
+ * @created 26/02/2023 - 11:47 AM
+ * @PROJECT java-parallelprogrammming
+ */
+public class ReciprocalarraySum {
+}
